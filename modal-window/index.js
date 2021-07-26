8bebe65cf3c35bd75b7ef0d0b02003c7b9a600a6
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-07-26 22:24:31 +0900

    クリックした記事のimg要素のsrc属性を取得
    .attr()で引数に指定した属性を取得できる。

--- modal-window/index.js
+++ modal-window/index.js
@@ -2,11 +2,13 @@ $(function() {
   let $article = $("article");
   let $modal_title = $(".modal-title");
   let $modal_content = $(".modal-content");
+  let $modal_img = $(".modal-img");
   $article.on("click", function() {
     let $title = $(this).find(".article-container h2").text();
     let $content = $(this).find(".article-container .article-content").text();
+    let $img = $(this).find(".article-image img").attr("src");
     $modal_title.text($title);
     $modal_content.text($content);
-    console.log($modal_content.text());
+    console.log($img);
   })
 })
