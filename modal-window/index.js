89d8333a79861f2915be4db8af891d16c749d8e1
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-07-26 22:07:12 +0900

    .modal-titleのテキストをクリックした記事のタイトルに書き換え

--- modal-window/index.js
+++ modal-window/index.js
@@ -4,6 +4,7 @@ $(function() {
   let $modal_title = $(".modal-title");
   $article.on("click", function() {
     let $title = $(this).find(".article-container h2").text();
-    console.log($title);
+    $modal_title.text($title)
+    console.log($modal_title.text());
   })
 })
