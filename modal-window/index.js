0aba4745849ee1155417f9378ac4d4788bb9141d
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-07-26 22:12:14 +0900

    クリックした記事のコンテンツ(.article-content)を取得

--- modal-window/index.js
+++ modal-window/index.js
@@ -1,10 +1,11 @@
 $(function() {
   let $article = $("article");
-  let $modal = $(".modal");
   let $modal_title = $(".modal-title");
+  let $modal_content = $(".modal-content");
   $article.on("click", function() {
     let $title = $(this).find(".article-container h2").text();
+    let $content = $(this).find(".article-container .article-content").text();
+    console.log($content);
     $modal_title.text($title)
-    console.log($modal_title.text());
   })
 })
