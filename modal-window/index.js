8d6e32eff3c9078c49e4ac1fade4889631940057
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-07-26 23:27:31 +0900

    モダールウィンドウを「CLOSE」ボタンまたはウィンドウ外をクリックすることで閉じる

--- modal-window/index.js
+++ modal-window/index.js
@@ -4,6 +4,9 @@ $(function() {
   let $modal_title = $(".modal-title");
   let $modal_content = $(".modal-content");
   let $modal_img = $(".modal-img");
+  let $modal_bg = $(".modal-bg");
+  let $modal_close_btn = $(".modal-close-btn");
+
   $article.on("click", function() {
     let $title = $(this).find(".article-container h2").text();
     let $content = $(this).find(".article-container .article-content").text();
@@ -13,5 +16,13 @@ $(function() {
     $modal_content.text($content);
     $modal_img.attr("src", $img);
     $modal.css("display", "block");
-  })
+  });
+
+  $modal_bg.on("click", function() {
+    $modal.css("display", "none");
+  });
+
+  $modal_close_btn.on("click", function() {
+    $modal.css("display", "none");
+  });
 })
