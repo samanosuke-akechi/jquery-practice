46a8fa4cd79a5336098d9f6668284ed3bda30918
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-07-26 22:51:11 +0900

    記事をクリックしたときにモーダルウィンドウを表示
    モーダルウィンドウの.modalにかけられているdisplay: none;をdisplay: block;に変更することで、モーダルウィンドウを表示させている。

--- modal-window/index.js
+++ modal-window/index.js
@@ -1,5 +1,6 @@
 $(function() {
   let $article = $("article");
+  let $modal = $(".modal");
   let $modal_title = $(".modal-title");
   let $modal_content = $(".modal-content");
   let $modal_img = $(".modal-img");
@@ -7,9 +8,10 @@ $(function() {
     let $title = $(this).find(".article-container h2").text();
     let $content = $(this).find(".article-container .article-content").text();
     let $img = $(this).find(".article-image img").attr("src");
+
     $modal_title.text($title);
     $modal_content.text($content);
     $modal_img.attr("src", $img);
-    console.log($modal_img.attr("src"));
+    $modal.css("display", "block");
   })
 })
