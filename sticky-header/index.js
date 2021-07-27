e33bc11533ed7a6f25a1f8a5885f66fe0d6a1d97
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-07-27 18:11:17 +0900

    ヘッダーに対するウィンドウの座標位置に応じて、headerにactiveクラスを付け外しする
    ヘッダーの座標よりウィンドウの座標が大きい(ヘッダーの位置より下にスクロールしている)場合にactiveクラスが付与。
    ヘッダーの座標よりウィンドウの座標が小さい(ヘッダーの位置より上にスクロールしている)場合にactiveクラスが外れる。

--- sticky-header/index.js
+++ sticky-header/index.js
@@ -4,6 +4,11 @@ $(function() {
 
   $(window).on("scroll", function() {
     let absolute_coord = $("html, body").scrollTop();
-    console.log(absolute_coord);
+    
+    if ($header_coord.top < absolute_coord) {  // ヘッダー座標よりウィンドウのトップ座標が大きい(ヘッダーの位置より下にスクロールしている)場合
+      $header.addClass("active");
+    } else {
+      $header.removeClass("active");
+    };
   })
 })
